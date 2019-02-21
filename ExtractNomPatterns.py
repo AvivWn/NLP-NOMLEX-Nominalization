2c9fd48a6b3fbe55dd70888cfdf83153fad6e95d
Aviv Weinstein <aviv.wn@gmail.com>
2019-02-21 17:08:36 +0200

    catching also single patterns

--- ExtractNomPatterns.py
+++ ExtractNomPatterns.py
@@ -22,24 +22,61 @@ def get_pairs(entry):
 	:return: a list of all possible pairs for a specific nominalization entry (list of tuples)
 	"""
 
-	default_subjects = list(entry.get("VERB-SUBJ", {}).keys())
-	default_subjects.append("by")
-	pairs = []
+	# Getting the default subject roles
+	default_subjects = list(entry.get("VERB-SUBJ", {"NOM-IS-SUBJ": {}}).keys())
+	default_subjects.append("PP-BY")
 
+	# Checking if NOT-PP-BY appeared
 	for subj in default_subjects:
 		if subj == "NOT-PP-BY":
-			default_subjects.remove("by")
+			default_subjects.remove("PP-BY")
+			default_subjects.remove("NOT-PP-BY")
+
+	patterns = list(itertools.product(default_subjects, ["NONE"]))
+
+	required_list = list(entry.get("REQUIRED", {}).keys())
+
 
 	for subcat, subcat_info in entry.get("VERB-SUBC", {}).items():
-		objects = list(subcat_info.get("OBJECT", {}).keys())
-		subjects = list(subcat_info.get("SUBJECT", {}).keys())
+		objects_subentry = subcat_info.get("OBJECT", {})
+		subjects_subentry = subcat_info.get("SUBJECT", {})
 
-		if subjects == []:
-			subjects = default_subjects
+		if objects_subentry != "NONE" and subjects_subentry != "NONE":
+			objects = list(objects_subentry.keys())
+			subjects = list(subjects_subentry.keys())
 
-		pairs += list(itertools.product(subjects, objects))
+			subjects.append("PP-BY")
+			# Checking if NOT-PP-BY appeared
+			for subj in subjects:
+				if subj == "NOT-PP-BY":
+					subjects.remove("PP-BY")
+					subjects.remove("NOT-PP-BY")
 
-	return pairs
+			if subjects == []:
+				subjects = default_subjects
+
+			if "SUBJECT" not in required_list:
+				patterns += list(itertools.product(["NONE"], objects_subentry.keys()))
+
+			if "OBJECT" not in required_list:
+				patterns += list(itertools.product(subjects_subentry.keys(), ["NONE"]))
+
+			patterns += list(itertools.product(subjects, objects))
+		elif objects_subentry != "NONE":
+			patterns += list(itertools.product(["NONE"], objects_subentry.keys()))
+		elif subjects_subentry != "NONE":
+			subjects = list(subjects_subentry.keys())
+
+			subjects.append("PP-BY")
+			# Checking if NOT-PP-BY appeared
+			for subj in subjects:
+				if subj == "NOT-PP-BY":
+					subjects.remove("PP-BY")
+					subjects.remove("NOT-PP-BY")
+
+			patterns += list(itertools.product(subjects, ["NONE"]))
+
+	return patterns
 
 
 def extract_nom_patterns(json_file_name):
