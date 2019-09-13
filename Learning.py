0620b9fa70d365700ff2f400f8654e08545848e7
Aviv Weinstein <aviv.wn@gmail.com>
2019-09-13 13:14:33 +0300

    Trying two different models for arguments predictions

---
+++ Learning.py
@@ -0,0 +1,446 @@
+import operator
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+from collections import defaultdict
+#from matplotlib import pyplot as plt
+
+from DictsAndTables import get_subentries_table
+from Model import my_model, my_model2
+
+# Constants
+tags_dict = dict([(tag.upper(), i + 4) for i, (tag, _, _) in enumerate(get_subentries_table())] + [("NONE", 0), ("NOM", 1), ("NOM_SUBJECT", 2), ("NOM_OBJECT", 3)])
+BATCH_SIZE = 64
+TRAIN_LIMIT_SIZE = 10000
+TEST_LIMIT_SIZE = 10000
+MODEL_NUM = 2
+
+# Hyper Parameters
+num_of_epochs = 10
+learning_rate = 0.001
+momentum_factor = 0.75
+
+tokenizer = torch.hub.load('huggingface/pytorch-pretrained-BERT', 'bertTokenizer', 'bert-base-cased', do_basic_tokenize=False)
+trained_model_filename = "trained_model"
+
+
+
+def encode_tags(tags):
+	tags_idx = []
+
+	for i in range(len(tags)):
+		tags_idx.append(tags_dict.get(tags[i], -1))
+
+	return tags_idx
+
+def raw_to_batch_examples(sent, splitted_tags_idxs, sent_idx):
+	batch_examples = []
+
+	right_tags_list = splitted_tags_idxs["+"]
+
+	if len(right_tags_list) == 0:
+		return []
+
+	right_tags_idxs = splitted_tags_idxs["+"][0]
+	wrong_tags_idxs_list = splitted_tags_idxs["-"]
+	splitted_sent = sent.split(" ")
+
+	if MODEL_NUM == 1:
+		batch_examples.append((sent_idx, splitted_sent, right_tags_idxs, len(splitted_sent), True))
+
+	else: #if MODEL_NUM == 2:
+		batch_examples.append((sent_idx, splitted_sent, right_tags_idxs, len(splitted_sent), True))
+
+		for wrong_tags_idxs in wrong_tags_idxs_list:
+			batch_examples.append((sent_idx, splitted_sent, wrong_tags_idxs, len(splitted_sent), False))
+
+	return batch_examples
+
+def forward(batch_examples, model, device):
+	max_length = max(batch_examples, key=operator.itemgetter(3))[3]
+
+	# Sorting by sentence length first and then by sentence index
+	list.sort(batch_examples, key=operator.itemgetter(3, 0, 4), reverse=True)
+
+	# Adding padding to the sentences in the batch
+	padded_batch_tags = []
+	padded_batch_indexed_tokens = []
+	sents_lengths = []
+	for _, splitted_sent, tags_idxs, sent_length, _ in batch_examples:
+		padding_length = max_length - sent_length
+		padded_batch_tags.append(tags_idxs + [tags_dict.get("NONE", -1)] * padding_length)
+
+		sents_lengths.append(sent_length)
+		padded_batch_sent = splitted_sent + ["<PAD>"] * padding_length
+		padded_batch_indexed_tokens.append(tokenizer.convert_tokens_to_ids(padded_batch_sent))
+
+	# Transform the inputs into tensors, before give it to the model
+	padded_batch_indexed_tokens = torch.tensor(padded_batch_indexed_tokens).to(device)
+	sents_lengths = torch.tensor(sents_lengths).to(device)
+
+	batch_losses = []
+
+	# Calculating loss differenty for each model
+	if MODEL_NUM == 1:
+		# Calling the model to get the output
+		outputs = model(padded_batch_indexed_tokens, sents_lengths).permute(0, 2, 1)
+
+		# Calculating the loss
+		padded_batch_tags = torch.Tensor(padded_batch_tags).long().to(device)
+		batch_losses.append(torch.nn.functional.nll_loss(outputs, padded_batch_tags))
+	else: #if MODEL_NUM == 2:
+		# Calling the model to get the output
+		outputs = model(padded_batch_indexed_tokens, torch.tensor(padded_batch_tags).to(device), sents_lengths)
+
+		right_score = None
+		other_best_score = None
+		last_sent_idx = -1
+
+		output_by_sents = []
+		curr_outputs_for_sent = []
+
+		for ex_idx in range(len(batch_examples)):
+			sent_idx, splitted_sent, tags_idx, sent_length, is_right_tags = batch_examples[ex_idx]
+
+			if last_sent_idx == -1:
+				last_sent_idx = sent_idx
+
+			# Is it a new sentence, or the last one?
+			if sent_idx != last_sent_idx:
+				if type(other_best_score) == torch.Tensor and type(right_score) == torch.Tensor:
+					loss = torch.Tensor.max(torch.Tensor([0]).to(device),
+											torch.Tensor([1]).to(device) - (right_score - other_best_score))
+					batch_losses.append(loss)
+
+				output_by_sents.append((curr_outputs_for_sent, right_score))
+
+				last_sent_idx = sent_idx
+				right_score = None
+				other_best_score = None
+				curr_outputs_for_sent = []
+
+			curr_outputs_for_sent.append(outputs[ex_idx])
+
+			if is_right_tags:
+				right_score = outputs[ex_idx]
+			else:
+				if type(other_best_score) != torch.Tensor or \
+						not torch.Tensor.equal(torch.Tensor.max(torch.Tensor([0]).to(device),
+																outputs[0] - other_best_score[0]),
+											   torch.Tensor([0]).to(device)):
+					other_best_score = outputs[ex_idx]
+
+		outputs = output_by_sents
+
+	return batch_losses, outputs
+
+def get_acc(batch_examples, outputs):
+	batch_acc = 0
+
+	if MODEL_NUM == 1:
+		preds = outputs.argmax(dim=1, keepdim=True).squeeze(dim=1)  # get the index of the max log-probability
+		preds = preds.cpu().numpy()
+
+		for i in range(len(preds)):
+			count_right = 0
+
+			for j in range(len(batch_examples[i][2])):
+				if preds[i][j] == batch_examples[i][2][j]:
+					count_right += 1
+
+			batch_acc += count_right / len(batch_examples[i][2])
+
+		batch_acc /= len(batch_examples)
+	else: # if MODEL_NUM == 2:
+		count_right = 0
+
+		for (curr_outputs_for_sent, right_score) in outputs:
+			if max(curr_outputs_for_sent) == right_score:
+				count_right += 1
+
+		batch_acc = count_right / len(outputs)
+
+	return batch_acc
+
+
+def train(model, device, train_dataset, optimizer, epoch):
+	"""
+	Trains the model for a single epoch
+	:param model: the model
+	:param device: the device where the computation will be on (cpu or cuda)
+	:param train_dataset: the training data
+	:param optimizer: the optimizer that will help training the model
+	:param epoch: the number of the current epoch
+	:return: None
+	"""
+
+	model.train()
+
+	random_indexes = np.arange(len(train_dataset))[:TRAIN_LIMIT_SIZE]
+	np.random.shuffle(random_indexes)
+	sent_count = 0
+	batch_examples = []
+
+	for sent_idx in random_indexes:
+		sent, splitted_tags_idxs = train_dataset[sent_idx]
+		sent_count += 1 # Counting the sentences (for progress printing)
+
+		# Aggregating the batch examples
+		batch_examples += raw_to_batch_examples(sent, splitted_tags_idxs, sent_idx)
+
+		if len(batch_examples) >= BATCH_SIZE:
+			optimizer.zero_grad()
+
+			# Calculatin the losses of the examples in the batch
+			batch_losses, _ = forward(batch_examples, model, device)
+
+			# If there was any positive loss, propogate it and update the model
+			if batch_losses != []:
+				loss = sum(batch_losses).to(device) / len(batch_losses)
+				loss.backward()
+				optimizer.step()
+
+				print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+					epoch, sent_count, len(random_indexes),
+					100. * sent_count / len(random_indexes), loss.item()))
+
+			batch_examples = []
+
+def valid(model, device, test_dataset, dataset_name):
+	"""
+	Checks the prediction of the model over the validaiton data
+	:param model: the trained model
+	:param device: the device where the computation will be on (cpu or cuda)
+	:param test_dataset: the validation data
+	:param dataset_name: the name of the dataset (for printing)
+	:return: loss, acc score
+	"""
+
+	model.eval()
+	test_loss = 0
+	test_acc = 0
+
+	random_indexes = np.arange(len(test_dataset))[:TEST_LIMIT_SIZE]
+	np.random.shuffle(random_indexes)
+	sent_count = 0
+	batch_examples = []
+	num_of_batches = 0
+
+	with torch.no_grad():
+		for sent_idx in random_indexes:
+			sent, tags_dict = test_dataset[sent_idx]
+			sent_count += 1  # Counting the sentences (for progress printing)
+
+			# Aggregating the batch examples
+			batch_examples += raw_to_batch_examples(sent, tags_dict, sent_idx)
+
+			if len(batch_examples) >= BATCH_SIZE:
+				num_of_batches += 1
+
+				# Calculatin the losses of the examples in the batch
+				batch_losses, outputs = forward(batch_examples, model, device)
+
+				# If there was any positive loss, propogate it and update the model
+				if batch_losses != []:
+					loss = sum(batch_losses).to(device) / len(batch_losses)
+
+					# Sum up current loss
+					test_loss += loss.item()
+
+					# Calculate the accuracy of the batch
+					test_acc += get_acc(batch_examples, outputs)
+
+				batch_examples = []
+
+	test_loss /= num_of_batches
+	test_acc /= num_of_batches
+
+	print(dataset_name + ' set: Average loss: {:.4f}, ACC: {:.3f}'.format(test_loss, test_acc))
+
+	return test_loss, test_acc
+
+def test(model, device, test_dataset):
+	"""
+	Predicts over the test set, and writing the results in a suitable file
+	:param model: the trained model
+	:param device: the device where the computation will be on (cpu or cuda)
+	:param test_dataset: the test set
+	:return: None
+	"""
+
+	model.eval()
+
+	predictions = []
+
+	with torch.no_grad():
+		for sent in test_dataset:
+			output = model(sent, device)
+
+			pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+			predictions.append(pred)
+
+	return predictions
+
+
+def text_to_tags(sentence, arguments_as_text):
+	splitted_sent = sentence.split(" ")
+	tags = ["NONE"] * len(splitted_sent)
+
+	argumets = arguments_as_text.split(" ")
+
+	for x in argumets:
+		arg = x.split("_")
+		tag = "_".join(arg[:-2])
+		start_idx = int(arg[-2])
+		end_idx = int(arg[-1])
+		idx = start_idx
+
+		while idx != end_idx + 1:
+			tags[start_idx] = tag
+			idx += 1
+
+	return tags
+
+def load_examples_file(file_name):
+	examples = []
+
+	with open(file_name, "r") as file:
+		lines = file.readlines()
+
+	sent = ""
+	splitted_tags_idxs = defaultdict(list)
+
+	for line in lines:
+		line = line.replace("\n", "").replace("\r", "")
+
+		# Is it a sentence or a list of arguments?
+		if line.startswith("#"):
+			if sent != "":
+				examples.append((sent, splitted_tags_idxs))
+
+			sent = line[2:]
+			splitted_tags_idxs = defaultdict(list)
+		elif line.startswith("+") or line.startswith("-"):
+			tags = text_to_tags(sent, line[2:])
+
+			splitted_tags_idxs[line[0]] += [encode_tags(tags)]
+
+	examples.append((sent, splitted_tags_idxs))
+
+	return examples
+
+
+
+def main(action, train_filename, valid_filename):
+	use_cuda = torch.cuda.is_available()
+	#print(use_cuda)
+	device = torch.device("cuda" if use_cuda else "cpu")
+	print(device)
+
+	if MODEL_NUM == 1:
+		model = my_model(len(tags_dict.keys()))
+	else: #if MODEL_NUM == 2:
+		model = my_model2(len(tags_dict.keys()))
+
+	net = torch.nn.DataParallel(model, device_ids=[0,1,2,3]).to(device)
+	# There are only two possible actions- train and test
+	if action == "-train":
+		train_dataset = load_examples_file(train_filename)
+		valid_dataset = load_examples_file(valid_filename)
+
+		print("Train set size: ", len(train_dataset))
+		print("Validation set size: ", len(valid_dataset))
+
+		#optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum_factor)
+		optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+		train_losses = []
+		train_accs = []
+		valid_losses = []
+		valid_accs = []
+
+		for epoch in range(1, num_of_epochs + 1):
+			# Training the model for an epoch
+			train(net, device, train_dataset, optimizer, epoch)
+			print("")
+
+			# Validation on train and development sets
+			train_loss, train_acc = valid(net, device, train_dataset, "Train")
+			train_losses.append(train_loss)
+			train_accs.append(train_acc)
+
+			valid_loss, valid_acc = valid(model, device, valid_dataset, "Validation")
+			valid_losses.append(valid_loss)
+			valid_accs.append(valid_acc)
+			print("")
+
+			# Saving the trained models
+			torch.save(net.state_dict(), trained_model_filename)
+
+			# Saving the losses and the acc scores over training
+			np.savetxt("train_losses", np.array(train_losses))
+			np.savetxt("valid_losses", np.array(valid_losses))
+			np.savetxt("train_accs", np.array(train_accs))
+			np.savetxt("valid_accs", np.array(valid_accs))
+
+		# Getting test results of the trained model
+		#test(model, device, test_loader)
+
+		# Showing the losses and acc graphs, over the number of epochs
+		show_graph(train_accs, valid_accs, "Epoch Number", "AVG ACC")
+		show_graph(train_losses, valid_losses, "Epoch Number", "CTC Loss")
+	elif action == "-test":
+		# Loading the trained model
+		model.load_state_dict(torch.load(trained_model_filename))
+
+		# Getting test results according the trained model
+		#test(model, device, test_loader)
+
+
+
+def show_graph(x_train_list, x_valid_list, x_label, y_label):
+	"""
+	Shows the graph of the given set (x, y)
+	:param x_train_list: list of data from train
+	:param x_valid_list: list of data from validation
+	:param x_label: the x axis name
+	:param y_label: the y axis name
+	:return: None
+	"""
+	"""
+
+	if type(x_train_list) != list:
+		x_train_list = [x_train_list]
+
+	if type(x_valid_list) != list:
+		x_valid_list = [x_valid_list]
+
+	plt.plot(range(len(x_train_list)), x_train_list, label="Train (" + str(round(x_train_list[-1], 3)) + ")")
+	plt.plot(range(len(x_valid_list)), x_valid_list, label="Validation (" + str(round(x_valid_list[-1], 3)) + ")")
+	plt.xlabel(x_label)
+	plt.ylabel(y_label)
+	plt.legend(loc='upper right')
+	plt.show()
+	"""
+
+if __name__ == '__main__':
+	# command line arguments:
+	#	-train train_filename dev_filename
+	#	-test train_filename dev_filename
+	import sys
+
+	print(tags_dict)
+
+	if len(sys.argv) == 4:
+		main(sys.argv[1], sys.argv[2], sys.argv[3])
+	elif sys.argv[1] == "-graph":
+		# Loading losses and accs files
+		train_losses = np.loadtxt("train_losses").tolist()
+		valid_losses = np.loadtxt("valid_losses").tolist()
+		train_accs = np.loadtxt("train_accs").tolist()
+		valid_accs = np.loadtxt("valid_accs").tolist()
+
+		# Showing the losses and accs graphs
+		show_graph(train_accs, valid_accs, "Epoch Number", "AVG ACC")
+		show_graph(train_losses, valid_losses, "Epoch Number", "CTC Loss")
---
+++ Model.py
@@ -0,0 +1,85 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+from pytorch_transformers import *
+
+class my_model(nn.Module):
+
+	def __init__(self, tagset_size):
+		super(my_model, self).__init__()
+
+		self.bert = BertModel.from_pretrained('bert-base-uncased')
+
+		#self.fc = nn.Linear(768, 100, 100)
+
+		self.lstm = nn.LSTM(input_size=768, hidden_size=50 ,num_layers=1, batch_first=True, bidirectional=True, dropout=0.25)
+
+		self.fc1 = nn.Linear(100, 100, 100)
+		self.fc2 = nn.Linear(100, tagset_size, tagset_size)
+
+		nn.init.xavier_normal_(self.fc1.weight)
+		nn.init.xavier_normal_(self.fc2.weight)
+
+	def forward(self, padded_batch_indexed_tokens, sents_lengths):
+		# Moving to bert embeddings
+		bert_output = self.bert(padded_batch_indexed_tokens)
+
+		# RNN
+		packed_out = torch.nn.utils.rnn.pack_padded_sequence(bert_output[0], sents_lengths, batch_first=True)
+		lstm_out = self.lstm(packed_out)[0]
+		padded_out, _ = torch.nn.utils.rnn.pad_packed_sequence(lstm_out, batch_first=True, total_length=padded_batch_indexed_tokens.shape[1])
+
+		# Fully Connected
+		fc1_out = F.relu(self.fc1(padded_out))
+		fc2_out = self.fc2(fc1_out)
+
+		return F.log_softmax(fc2_out, dim=2)
+
+
+
+
+class my_model2(nn.Module):
+
+	def __init__(self, tagset_size):
+		super(my_model2, self).__init__()
+
+		self.tag_embedding = nn.Embedding(tagset_size, tagset_size)
+
+		self.tokenizer = torch.hub.load('huggingface/pytorch-pretrained-BERT', 'bertTokenizer', 'bert-base-cased', do_basic_tokenize=False)
+		self.bert = BertModel.from_pretrained('bert-base-uncased')
+
+		self.lstm = nn.LSTM(input_size=768 + tagset_size, hidden_size=150 ,num_layers=1, batch_first=True, bidirectional=True, dropout=0.25)
+
+		self.fc1 = nn.Linear(300, 100, 100)
+		self.fc2 = nn.Linear(100, 1, 1)
+
+		#nn.init.xavier_normal_(self.lstm.all_weights)
+		nn.init.xavier_normal_(self.fc1.weight)
+		nn.init.xavier_normal_(self.fc2.weight)
+
+	def forward(self, padded_batch_indexed_tokens, padded_batch_tags, sents_lengths):
+		# Moving to bert embeddings
+		bert_output = self.bert(padded_batch_indexed_tokens)
+
+		# Adding the conditions according to the given tags
+		conditions = self.tag_embedding(padded_batch_tags)
+		conditioned_bert = torch.cat([conditions, bert_output[0]], dim=2)
+
+		# RNN
+		if self.lstm.bidirectional:
+			num_directions = 2
+		else:
+			num_directions = 1
+
+		packed_out = torch.nn.utils.rnn.pack_padded_sequence(conditioned_bert, sents_lengths, batch_first=True)
+		lstm_out = self.lstm(packed_out)[1][0]
+
+		# Rearranging lstm output (we want only the last hidden state of the last layer)
+		lstm_out = lstm_out.view(self.lstm.num_layers, num_directions, lstm_out.shape[1], self.lstm.hidden_size)[-1]
+		lstm_out = torch.cat([lstm_out[0], lstm_out[1]], dim=1)
+
+		# Fully Connected
+		fc1_out = F.relu(self.fc1(lstm_out))
+		fc2_out = self.fc2(fc1_out).squeeze(dim=0).squeeze(dim=1)
+
+		return fc2_out
