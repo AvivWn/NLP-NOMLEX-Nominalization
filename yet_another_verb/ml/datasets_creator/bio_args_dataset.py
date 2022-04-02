c44a65f2b1a7b50fc15c25e7c284dd2a187cf033
Aviv <aviv.wn@gmail.com>
2022-04-02 18:12:21 +0300

    more options for bio-tagging dataset, including without-O, with-v1

--- yet_another_verb/ml/datasets_creator/bio_args_dataset.py
+++ yet_another_verb/ml/datasets_creator/bio_args_dataset.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from collections import namedtuple
 
 import pandas as pd
@@ -7,19 +7,59 @@ from tqdm import tqdm
 from yet_another_verb.arguments_extractor.extraction.multi_word_extraction import MultiWordExtractions
 from yet_another_verb.arguments_extractor.extraction.representation.bio_representation import BIORepresentation
 from yet_another_verb.dependency_parsing.dependency_parser.parsed_text import ParsedText
+from yet_another_verb.dependency_parsing.dependency_parser.parsed_word import ParsedWord
 from yet_another_verb.file_handlers import PKLFileHandler, CSVFileHandler
 from yet_another_verb.ml.datasets_creator.dataset_creator import DatasetCreator
+from yet_another_verb.word_to_verb.verb_translator import VerbTranslator
 
 BIOTaggedSentence = namedtuple("BIOTaggedSentence", "predicate words labels")
 
 
 class BIOArgsDatasetCreator(DatasetCreator):
-	def __init__(self, in_dataset_path: str, dataset_size=None, limited_postags=None, limited_types=None, **kwargs):
+	def __init__(
+			self, in_dataset_path: str, verb_translator: VerbTranslator,
+			dataset_size=None, limited_postags=None, limited_types=None, use_base_verb=False,
+			replace_in_sentence=False, avoid_outside_tag=False,
+			**kwargs):
 		super().__init__(dataset_size)
 		self.in_dataset_path = in_dataset_path
+		self.verb_translator = verb_translator
 		self.limited_postags = limited_postags
 		self.limited_types = limited_types
+		self.use_base_verb = use_base_verb
+		self.replace_in_sentence = replace_in_sentence
+		self.avoid_outside_tag = avoid_outside_tag  # avoid O
+
 		self.bio_representation = BIORepresentation()
+
+	def _generate_bio_tagged_sentence(
+			self, words: ParsedText, predicate_idx: int, bio_tags: List[str]) -> Optional[BIOTaggedSentence]:
+		predicate = words[predicate_idx].lemma
+		if self.use_base_verb:
+			predicate = self.verb_translator.translate(predicate)
+
+		tokens = words.words
+		if self.replace_in_sentence:
+			tokens[predicate_idx] = predicate
+
+		if self.avoid_outside_tag:
+			relevant_idxs = [i for i in range(len(bio_tags)) if bio_tags[i] != 'O']
+			tokens = [tokens[i] for i in range(len(tokens)) if i in relevant_idxs]
+			bio_tags = [bio_tags[i] for i in range(len(bio_tags)) if i in relevant_idxs]
+
+		if len(tokens) == 0:
+			return
+
+		return BIOTaggedSentence(predicate, " ".join(tokens), " ".join(bio_tags))
+
+	def _should_filter(self, predicate: ParsedWord) -> bool:
+		if self.limited_postags is not None and predicate.tag not in self.limited_postags:
+			return True
+
+		if not self.verb_translator.is_transable(predicate.lemma):
+			return True
+
+		return False
 
 	def extractions_to_bio_tags(self, multi_word_extractions: MultiWordExtractions) -> List[BIOTaggedSentence]:
 		bios = []
@@ -35,8 +75,11 @@ class BIOArgsDatasetCreator(DatasetCreator):
 			)
 
 			for predicate_idx, bio_tags in bio_tags_by_predicate.items():
-				if self.limited_postags is None or words[predicate_idx].tag in self.limited_postags:
-					bios.append(BIOTaggedSentence(words[predicate_idx], words.tokenized_text, " ".join(bio_tags)))
+				if not self._should_filter(words[predicate_idx]):
+					tagged_sentence = self._generate_bio_tagged_sentence(words, predicate_idx, bio_tags)
+
+					if tagged_sentence is not None:
+						bios.append(tagged_sentence)
 
 		return bios
 
--- yet_another_verb/ml/datasets_creator/create_dataset.py
+++ yet_another_verb/ml/datasets_creator/create_dataset.py
@@ -1,13 +1,14 @@
 from argparse import ArgumentParser, Namespace
 
 from yet_another_verb.configuration.verbose_config import VERBOSE_CONFIG
+from yet_another_verb.factories.verb_translator_factory import VerbTranslatorFactory
 from yet_another_verb.ml.datasets_creator.dataset_creator import DatasetCreator
 from yet_another_verb.ml.datasets_creator import WikiDatasetCreator, ParsedDatasetCreator, ExtractedDatasetCreator, \
 	BIOArgsDatasetCreator
 from yet_another_verb.factories.dependency_parser_factory import DependencyParserFactory
 from yet_another_verb.factories.extractor_factory import ExtractorFactory
 from yet_another_verb.nomlex.constants.argument_type import PP_ARG_TYPES, NP_ARG_TYPES
-from yet_another_verb.nomlex.constants.word_postag import VERB_POSTAGS
+from yet_another_verb.nomlex.constants.word_postag import VERB_POSTAGS, NOUN_POSTAGS
 from yet_another_verb.utils.debug_utils import timeit
 
 DATASET_TO_CREATOR = {
@@ -19,10 +20,13 @@ DATASET_TO_CREATOR = {
 		**vars(args),
 		args_extractor=ExtractorFactory(**vars(args))(),
 		dependency_parser=DependencyParserFactory(**vars(args))()),
-	"verb-bio-args": lambda args: BIOArgsDatasetCreator(
+	"bio-args": lambda args: BIOArgsDatasetCreator(
 		**vars(args),
 		limited_postags=VERB_POSTAGS,
-		limited_types=NP_ARG_TYPES + PP_ARG_TYPES)
+		limited_types=NP_ARG_TYPES + PP_ARG_TYPES,
+		use_base_verb=True,
+		avoid_outside_tag=True,
+		verb_translator=VerbTranslatorFactory(**vars(args))()),
 }
 
 
@@ -48,6 +52,7 @@ def main():
 
 	DependencyParserFactory.expand_parser(arg_parser)
 	ExtractorFactory.expand_parser(arg_parser)
+	VerbTranslatorFactory.expand_parser(arg_parser)
 
 	args, _ = arg_parser.parse_known_args()
 	VERBOSE_CONFIG.VERBOSE = args.verbose
